e84e2ea672a1ee649259a801da5d034322cb6ee4
Karsten Jepsen <kjepsen86@gmail.com>
2021-05-24 16:30:16 -0500

    set up database,  added test data, updated .sql

--- database.sql
+++ database.sql
@@ -8,3 +8,53 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "users" (
+	"id" SERIAL PRIMARY KEY,
+	"username" VARCHAR (80) UNIQUE NOT NULL,
+	"password" VARCHAR (80) NOT NULL,
+	"number" INTEGER
+);
+
+CREATE TABLE "batch" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (60),
+	"batch_#" INTEGER,
+	"tank_#" INTEGER,
+	"complete" BOOLEAN DEFAULT FALSE,
+	"user_id" INT REFERENCES "users" NOT NULL
+);
+
+CREATE TABLE "hop_additions" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (60) NOT NULL,
+	"amount" INTEGER NOT NULL,
+	"unit" VARCHAR (10) NOT NULL,
+	"date" DATE,
+	"complete" BOOLEAN DEFAULT FALSE,
+	"batch_id" INT REFERENCES "batch" NOT NULL
+);
+
+INSERT INTO "users" (username, password, number)
+VALUES ('kjepsen86@gmail.com', 'karsten', '6514429080'),
+('karstenjepsen@q.com', 'karsten2', '6514429080');
+
+INSERT INTO "users" ("username", "password", "number")
+VALUES ('kjepsen86@gmail.com', 'karsten', 6514429080),
+('karstenjepsen@q.com', 'karsten2', 6514429080);
+
+INSERT INTO "batch" ("name", "batch_#", "tank_#", "user_id")
+VALUES ('Cabin Daze IPA', 333, 8, 1),
+('Bad Axe DIPA', 334, 11, 1),
+('Fine! IPA', 335, 4, 2),
+('Sweet Digs NEIPA', 336, 13, 2);
+
+INSERT INTO "hop_additions" ("name", "amount", "unit", "date", "batch_id")
+VALUES ('Citra', 80, 'oz', '5-31-2021', 1),
+('Citra', 60, 'oz', '6-2-2021', 1),
+('Centennial', 5, 'lbs', '5-30-2021', 2),
+('Mosaic', 60, 'oz', '6-3-2021', 3),
+('Azacca', 40, 'oz', '6-6-2021', 3),
+('Julius', 50, 'oz', '6-6-2021', 3),
+('Strata', 100, 'oz', '6-1-2021', 4);
+
