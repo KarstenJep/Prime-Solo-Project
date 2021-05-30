bcc496617a6b6237e44f1639afc73627a8593c21
Karsten Jepsen <kjepsen86@gmail.com>
2021-05-30 17:20:25 -0500

    added Inventory saga

---
+++ src/redux/sagas/inventory.saga.js
@@ -0,0 +1,14 @@
+import { put } from '@redux-saga/core/effects';
+import axios from 'axios';
+
+function* inventory() {
+    try {
+        const inventory = yield axios.get('/api/batch');
+        console.log('fetch inventory', inventory.data);
+        yield put({type: 'SET_INVENTORY', payload: inventory.data});
+    } catch {
+        console.log('get inventory error');
+    }
+}
+
+export default inventory;
