cb5149da629501239a1a898ea514a0e03b6b6d70
Karsten Jepsen <kjepsen86@gmail.com>
2021-06-01 19:14:35 -0500

    Added material ui to HopForm

--- src/components/HopForm/HopForm.jsx
+++ src/components/HopForm/HopForm.jsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-// import DatePicker from "react-datepicker";
-// import "react-datepicker/dist/react-datepicker.css";
+
 
 function HopForm() {
     const [hopName, setHopName] = useState('');
@@ -17,19 +24,31 @@ function HopForm() {
     const [dateError, setDateError] = useState(false);
     const dispatch = useDispatch();
 
+    let theme = createMuiTheme();
+
     const useStyles = makeStyles((theme) => ({
         root: {
           '& > *': {
             margin: theme.spacing(.5),
-            width: '75%',
-            height: '50%',
-            
+            // padding: theme.spacing(.5),
+            flexGrow: 1,
           },
         },
+        date: {
+            width:'90%',
+            padding: theme.spacing(.5),
+            textAlign: 'center',
+            color: theme.palette.text.secondary,
+        },
+          paper: {
+            padding: theme.spacing(.5),
+            textAlign: 'center',
+            color: theme.palette.text.primary,
+          },
       }));
       
     //   export default function HopForm() {
-        const classes = useStyles();
+    const classes = useStyles();
 
     const validateForm = (e) => {
         e.preventDefault();
@@ -46,29 +65,16 @@ function HopForm() {
         if (date == ''){
             setDateError(true)
         }
-        if (hopName && amount && date) {
+        if (unit == ''){
+            setUnitError(true)
+        }
+        if (hopName && amount && date && unit) {
             addHops(e);
         }
     }
 
     const addHops = (e) => {
-        // const isoDate = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
             e.preventDefault();
-           
-            // setHopNameError(false)
-            // setAmountError(false)
-            // setDateError(false)
-
-            // if (hopName == ''){
-            //     setHopNameError(true)
-            // }
-            // if (amount == ''){
-            //     setAmountError(true)
-            // }
-            // if (date == ''){
-            //     setDateError(true)
-            // }
-       
             console.log('Clicked add hops', hopName, amount, unit, date);
             dispatch({
                 type: 'SET_HOPS', payload: {
@@ -86,52 +92,71 @@ function HopForm() {
     }
 
     return (
-                <form autoComplete="off" minHeight onSubmit={validateForm}>
-                <h5>Hop Additions</h5>
-                    <TextField 
-                        className={classes.root}
-                        label="Hop Name"
-                        variant="outlined"
-                        value={hopName}
-                        onChange={(e) => setHopName(e.target.value)}
-                        error={hopNameError}
-                    />
-                    <TextField 
-                        className={classes.root}
-                        label="Amount"
-                        type="number"
-                        variant="outlined"
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                        error={amountError}
-                    />
-                    <TextField 
-                        className={classes.root}
-                        label="Unit"
-                        type="number"
-                        variant="outlined"
-                        value={unit}
-                        onChange={(e) => setUnit(e.target.value)}
-                    />
-                    <TextField 
-                        className={classes.root}
-                        // label="Date"
-                        type="date"
-                        variant="outlined"
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                        error={dateError}
-                    />
-                    
-                    {/* <input
-                        value={date}
-                        type="date"
-                        onChange={(e) => setDate(e.target.value)}
-                        /> */}
-                    <button type="submit">Add Hop Addition</button>
+        <>
+                <form className={classes.root} autoComplete="off" onSubmit={validateForm}>
+                <ThemeProvider theme={theme} className={classes.paper}>
+                    <Typography className={classes.paper} variant="h5"><b>Hop Additions</b></Typography>
+                </ThemeProvider>
+                <Grid container spacing={.25}>
+                    <Grid item xs={6}>
+                        <TextField 
+                            className={classes.paper}
+                            label="Hop Name"
+                            variant="outlined"
+                            value={hopName}
+                            onChange={(e) => setHopName(e.target.value)}
+                            error={hopNameError}
+                            size="small"
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField 
+                            className={classes.date}
+                            type="date"
+                            variant="outlined"
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                            error={dateError}
+                            size="small"
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField 
+                            className={classes.paper}
+                            label="Amount"
+                            type="number"
+                            variant="outlined"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                            error={amountError}
+                            size="small"
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField 
+                            className={classes.paper}
+                            select
+                            label="Unit"
+                            variant="outlined"
+                            value={unit}
+                            onChange={(e) => setUnit(e.target.value)}
+                            error={unitError}
+                            size="small"
+                        >
+                            <MenuItem value="oz">oz</MenuItem>
+                            <MenuItem value="lbs">lbs</MenuItem>
+                        </TextField>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Button type="submit" variant="contained" color="secondary" size="small" className={classes.root} padding=".5">
+                            Add Hop Addition
+                        </Button>
+                    </Grid>
+                </Grid>
+                   
                </form>
+               </>
     )
 }
-// }
 
 export default HopForm;
