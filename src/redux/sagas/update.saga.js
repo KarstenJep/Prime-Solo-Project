c5dc2d1659b4781c786fee94be785378d2ae2f71
Karsten Jepsen <kjepsen86@gmail.com>
2021-06-04 19:50:04 -0500

    Improved syntax

--- src/redux/sagas/update.saga.js
+++ src/redux/sagas/update.saga.js
@@ -5,11 +5,11 @@ function* updateBatch( action ) {
     // Sent updates to the DB via PUT
     console.log('in update saga', action.payload);
     try {
-        yield axios.put( `/api/batch/${action.payload}`, action.payload );
+        yield axios.put( `/api/update`, action.payload );
         // Get all batches from server, including update. Sets batch reducer.
         yield put({type: 'FETCH_BATCH'});
-        // Update the update reducer
-        yield put({type: 'SET_UPDATE', payload: action.payload });
+        // // Update the update reducer
+        // yield put({type: 'SET_UPDATE', payload: action.payload });
 
     } catch (error) {
         // alert(`Sorry. Things aren't working at the moment.`);
