acbf9f5624d98f813d973985a3281712102757dd
Karsten Jepsen <kjepsen86@gmail.com>
2021-06-04 19:45:57 -0500

    Added toggle inventory view to schedule page

--- src/components/SchedulePage/SchedulePage.jsx
+++ src/components/SchedulePage/SchedulePage.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Box from '@material-ui/core/Box';
+import InventoryPage from '../InventoryPage/InventoryPage';
+
 
 function Schedule() {
     const dispatch = useDispatch();
     const history = useHistory();
     const batches = useSelector((store) => store.batch);
+    const inventory = useSelector((store) => store.inventory);
+    const [inventoryView, setInventoryView] = useState(false);
+    const [additionView, setAdditionView] = useState(false);
 
     useEffect(() => {
         // on page load, get list of batches from the database
@@ -15,30 +23,57 @@ function Schedule() {
       const handleClick = (batch) => {
           console.log('clicked a batch', batch);
           dispatch({ type: 'SET_UPDATE', payload: batch });
-          history.push('/update');  
+          history.push(`/update/${batch.id}`);  
+      }
+
+      const handleInventory = () => {
+        setInventoryView(true)
+        setAdditionView(false)
+      }
+
+      const handleAdditions = () => {
+        setAdditionView(true)
+        setInventoryView(false)
       }
 
     return (
         <>
-        <h1>Schedule</h1>
+        { inventoryView === false &&
+        <Box ml={14}>
+            <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+                <Button onClick={handleInventory}>Inventory</Button>
+                <Button onClick={handleAdditions}>Additions</Button>
+            </ButtonGroup>
+        </Box>
+        }
+        
+        { inventory && inventoryView ?   
+        <InventoryPage />
+        :
+        <div className="formPanel3">
+        <h1>&nbsp;Schedule</h1>
+        
         
         {batches.map((batch, i) => {
             console.log('in schedule map', batch);
             return (
-                <ul key={i}>
-                    <li onClick={(e) => handleClick(batch)}>
-                        {batch.batch_num} - {batch.name} {batch.style} - Tank {batch.tank}
-                        </li>
+                <span key={i}>
+                    <p onClick={(e) => handleClick(batch)}>
+                        <b>&nbsp;{batch.batch_num}</b> - {batch.name} {batch.style} - Tank {batch.tank}
+                        </p>
                         {/* <p>Hop Additions: {batch.hops.map(dates => {
                             console.log('in date map', dates);
                             return (
                                 <p>{dates.date}</p>
                             )
                         })}</p> */}
-                </ul>
+                        </span>
             )
-        })}
-        </>
+        })} 
+        
+        </div>
+        }
+      </>  
     )       
 }
 
