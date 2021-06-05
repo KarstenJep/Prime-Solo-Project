9620d0c8301a113817bacb73839b0deade1a7837
Karsten Jepsen <kjepsen86@gmail.com>
2021-06-04 19:48:37 -0500

    Updated syntax

--- src/redux/reducers/update.reducer.js
+++ src/redux/reducers/update.reducer.js
@@ -1,14 +1,8 @@
 const updateReducer = (state = {}, action) => {
+    console.log('in UR', action);
     switch (action.type) {
         case 'SET_UPDATE':
             return action.payload;
-        case 'EDIT_ONCHANGE':
-            console.log('in edit reducer', action.payload);
-            return { // spread: give me all of the object (...state)
-                ...state,
-                // change this one in particular
-                [action.payload.property]: action.payload.value,
-            }
         default:
             return state;
     }
