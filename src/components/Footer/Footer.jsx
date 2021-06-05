87957063d211e946fe048a9279f86447493da2d9
Karsten Jepsen <kjepsen86@gmail.com>
2021-06-04 19:42:01 -0500

    Adjusted Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; K-GO 2021</footer>;
+  return <footer><b>&copy; Whats Hop'nin?</b></footer>;
 }
 
 export default Footer;
