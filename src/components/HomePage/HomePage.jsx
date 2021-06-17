39d00a08294fe948b2d6774246c0c18f85211c04
Karsten Jepsen <kjepsen86@gmail.com>
2021-06-16 23:14:28 -0500

    Added page refresh after completed addition

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,94 +1,58 @@
-import moment, { updateLocale } from 'moment';
 import React, { useEffect, useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box'
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(.5),
-      // padding: theme.spacing(.5),
-      flexGrow: 1,
-      itemAlign: 'center',
-      
-    },
-  },
-  button: {
-    itemAlign: 'center'
-  },
-    paper: {
-      padding: theme.spacing(.5),
-      textAlign: 'center',
-      color: theme.palette.text.primary,
-    },
-    
-    
-}));
-
+import moment from 'moment';
 
 function HomePage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
   const daily = useSelector((store) => store.daily);
-  const [date, setDate] = useState(new Date());
-  const weekday = moment().format('dddd');
-  const id = moment().format('YYYY-MM-DD');
-  const today = moment().format("MMMM Do");
-  const dispatch = useDispatch();
   const [complete, setComplete] = useState('false');
 
-  const classes = useStyles();
+  // Import and format current date via moment.js
+  const date = moment().format('YYYY-MM-DD');
+  const today = moment().format("dddd, MMMM Do");
   
-  console.log('in home', today, daily, user, date);
-  console.log("moment", moment().format('YYYY MM DD')); 
-  
+  // console.log('in home', today, daily, user);
+ 
+  // on page load, get list of daily hop additions from the database
   useEffect(() => {
-    // on page load, get list of daily hop additions from the database
-    dispatch({ type: 'FETCH_DAILY', payload: id });
-  }, []) // daily
+    dispatch({ type: 'FETCH_DAILY', payload: date });
+  }, []) 
 
+  // Click handler for marking a hop addition complete, and then refresh daily list
   const handleComplete = (id) => {
-    console.log('Clicked complete', id);
-    dispatch({ type: 'COMPLETE_HOP', payload: id,  });
+    // console.log('Clicked complete', id, date);
+    dispatch({ type: 'COMPLETE_HOP', payload: id, date });
   }
-
-  const checkCompleted = () => {
-    console.log('in checkCompleted', daily[0].complete);
-    for (let addition of daily) {
-      if(addition.complete === 'true') {
-        return console.log('Done');
-     }
-      if (addition.complete == 'false')
-        return console.log('yo');
-    }
-  }
-
 
   return (
-    <>   
-    <h2 className="formPanel5"><b>{weekday},<br></br>{today}</b></h2>
-    
-    {daily.map(addition => {
-      console.log('in home map', addition, addition.complete);
-      
-      return (
-        
-        <div className="formPanel2" key={addition.hop_id}>
-          
-          <h3><b>{addition.name} {addition.style}</b></h3>
-          <p><i> Tank: </i><b>{addition.tank}</b> / <i> Batch: </i><b>{addition.batch_num}</b></p>
+    <>
+      <div>
+        <h2 className="formPanel5"><b>{today}</b></h2>
+      </div>
 
-          <p><i> Hop:</i><b> {addition.hop_name}</b>&nbsp; 
-             / <i> Amount:</i> <b>{addition.amount} {addition.unit}</b> &nbsp;
-               <button onClick={() => handleComplete(addition.hop_id)}>✔️</button></p>
-        </div>
-      )
-    })}
+      {daily.map(addition => {
+        // console.log('in home map', addition);
+        if(addition.complete === false ) {
+          return (
+            <div className="formPanel2" key={addition.hop_id}>
+              <h3><b>{addition.name} {addition.style}</b></h3>
+              <p><i>Tank:</i> <b>{addition.tank}</b> / <i>Batch:</i> <b>{addition.batch_num}</b></p>
+              <p><i>Hop:</i> <b>{addition.hop_name}</b> / <i>Amount:</i> <b>{addition.amount} {addition.unit}</b>&nbsp;
+              <button onClick={() => handleComplete(addition.hop_id)}>✔️</button></p>
+            </div>
+          )
+      }
+        else {
+            return (
+              <div className="formPanel2" key={addition.hop_id}>
+                <h3><b>{addition.name} {addition.style}&nbsp; ✔️</b></h3> 
+              </div>
+            )
+        }
+      })}
     </>
   );
 }
-
 
 export default HomePage;
--- src/redux/sagas/complete.saga.js
+++ src/redux/sagas/complete.saga.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 function* completeHop( action ) {
     // Sent updates to the DB via PUT
-    console.log('in complete saga', action.payload);
+    console.log('in complete saga', action.payload, action);
     try {
         yield axios.put( `/api/update/complete/${action.payload}`, action.payload );
         // Get all batches from server, including update. Sets daily reducer.
-        // yield put({type: 'FETCH_DAILY'});
+        yield put({type: 'FETCH_DAILY', payload: action.date});
         // // Update the update reducer
         // yield put({type: 'SET_UPDATE', payload: action.payload });
 
