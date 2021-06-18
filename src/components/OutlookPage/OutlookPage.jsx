d781c41077fe7fe5dd6fc85532c47e59639b56d3
Karsten Jepsen <kjepsen86@gmail.com>
2021-06-18 15:17:01 -0500

    Improved naming, syntax

--- src/components/OutlookPage/OutlookPage.jsx
+++ src/components/OutlookPage/OutlookPage.jsx
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux';
-import BatchOutlook from '../BatchOutlook/BatchOutlook';
+import React, { useState } from 'react';
+import BatchView from '../BatchView/BatchView';
+import HopCountView from '../HopCountView/HopCountView';
+import AdditionsView from '../AdditionsView/AdditionsView';
+// Material UI Imports
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Box from '@material-ui/core/Box';
-import InventoryPage from '../InventoryPage/InventoryPage';
-import AdditionsOutlook from '../AdditionsOutlook/AdditionsOutlook';
 
 
 function Schedule() {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    // const batches = useSelector((store) => store.batch);
-    // const inventory = useSelector((store) => store.inventory);
+
     const [batchView, setBatchView] = useState(true);
     const [hopView, setHopView] = useState(false);
     const [additionView, setAdditionView] = useState(false);
 
+    // The 3 handlers below check state to determin which component/view to display
     const handleBatch = () => {
         setBatchView(true)
         setHopView(false)
@@ -28,7 +25,6 @@ function Schedule() {
         setBatchView(false)
         setHopView(true)
         setAdditionView(false)
-      
       }
 
       const handleAdditions = () => {
@@ -38,27 +34,27 @@ function Schedule() {
       }
 
     return (
-        <>
+      <>
+        {/* Button group changes state to control user view */}
         <Box ml={6}>
-            <ButtonGroup variant="contained" aria-label="contained primary button group">
+            <ButtonGroup variant="contained" aria-label="contained primary button group" >
                 <Button color="primary" onClick={handleBatch}>Batch</Button>
                 <Button color="secondary" onClick={handleAdditions}>Additions</Button>
-                <Button color="primary" type="dark" onClick={handleHops}>Hops</Button>
+                <Button color="primary" onClick={handleHops}>Hops</Button>
             </ButtonGroup>
         </Box>
         
         { hopView &&   
-        <InventoryPage />
+        <HopCountView />
         }
 
         { batchView &&
-        <BatchOutlook />
+        <BatchView />
         }
         
         { additionView &&
-        <AdditionsOutlook />
-        }
-        
+        <AdditionsView />
+        }   
       </>  
     )       
 }
